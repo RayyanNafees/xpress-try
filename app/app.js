296a9ccc8550abaf3ac2e0d0b54d1f9bfaeba7b4
RayyanNafees <nafees.rayyan@gmail.com>
2021-05-19 00:44:04 +0530

    trial

--- app/app.js
+++
@@ -1,6 +0,0 @@
-const express= require('express')
-const app = express()
-
-app.get('/', (_, res)=> res.send('It worked'))
-
-app.listen(process.env.PORT||8080, ()=> console.log('http://localhost:8080/')) 
